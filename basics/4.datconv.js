3578800195c9dbdcec9ea2c96b0e5887c7f0d7ac
avik211 <99727212+avik211@users.noreply.github.com>
2025-07-05 08:31:43 +0000

    1

---
+++ basics/4.datconv.js
@@ -0,0 +1,15 @@
+let score1=33
+let name1="avik"
+let bool=true
+console.log(typeof(score1));
+console.log(typeof(name1));
+console.log(typeof(bool));
+let numbers=Number(bool)
+console.log(typeof(numbers));
+console.log(numbers);
+let numtext=Number(name1)
+console.log(typeof(numtext));
+console.log(numtext);
+//like wise we can also use Boolean to convert into boolean values and string vlaues it somewhat like type casting in c
+
+
